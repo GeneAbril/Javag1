372d5dbb4efa7a58de24a55b9064155a5f6ef424
Gene <genesisabril.qr@gmail.com>
2022-05-29 16:09:00 -0400

    Cambios gene2

---
+++ src/com/generation20222505/BucleWhile.java
@@ -0,0 +1,23 @@
+package com.generation20222505;
+
+import java.util.Scanner;
+
+public class BucleWhile {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);//0° ingresar scanner
+		System.out.println("Ingresar opcion (1) Entrar ; (0) Salir ");//crear sysou
+		int opcion= sc.nextInt();//2° capturar el ingreso por consola
+		
+//3° Valida que la condicion sea verdadera, mientras no se cumpla se debe repetir
+		while (opcion != 1 && opcion != 0) {// 4° Hacer la comparacion con || o &&
+			System.out.println("Ingresar opcion (1) Entrar ; (0) Salir"); //5° enviar respuesta a consola
+	opcion = sc.nextInt();//6° imprimir en consola
+}
+	
+System.out.println("Fuera del while");
+		
+
+	}
+
+}
